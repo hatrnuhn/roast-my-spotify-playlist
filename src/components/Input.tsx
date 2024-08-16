0c0848c793e5350e743bb38c798c28a6e2bbca78
hatrnuhn <yusron.ibnunaim@gmail.com>
2024-08-16 11:53:12 +0700

    updates input error handling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -28,22 +28,31 @@ const Input: FC<InputProps> = ({ pushFn }) => {
     const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault()
         setIsButtonDisabled(true)
-        setTimeout(() => setIsButtonDisabled(false), 1000)
+        setTimeout(() => setIsButtonDisabled(false), 2000)
 
         const url = isValidURL(input)
         let playlistId: string
 
-        if (url)
+        if (url) {
             playlistId = url.pathname.split('/')[2]
-        else 
+            if (!playlistId)
+                setErrorMsg('Invalid URL')
+        } else 
             playlistId = input
 
-        if (new RegExp('^[a-zA-Z0-9_-]{22}$').test(playlistId))
+        const isValidPlaylistId = new RegExp('^[a-zA-Z0-9_-]{22}$').test(playlistId);
+
+        if (isValidPlaylistId)
             pushFn(a => [...a, playlistId])
         else {
             if (inputRef.current)
                 inputRef.current.focus()
-            setErrorMsg(`${playlistId} is not a valid playlist ID`)
+            if (playlistId && playlistId.length > 22)
+                playlistId = playlistId.slice(0, 20).concat('...')
+
+            if (playlistId && !errorMsg)
+                setErrorMsg(`${playlistId} is not a valid playlist ID`)
+
             setTimeout(() => setErrorMsg(''), 3000)
         }
     }
@@ -64,10 +73,10 @@ const Input: FC<InputProps> = ({ pushFn }) => {
             <button type="button" onClick={() => switchLanguage()} className="uppercase text-sm font-bold min-w-5" aria-live="polite" aria-label={`Button: switch responses language`} disabled={isButtonDisabled}>
                 <span>{language}</span>
             </button>
-            <form onSubmit={onSubmit} className="flex grow gap-2.5 lg:gap-5 items-center relative">
+            <form onSubmit={isButtonDisabled ? undefined : onSubmit} className="flex grow gap-2.5 lg:gap-5 items-center relative">
                 <input type="text" name="input-id" placeholder="Enter your playlist URL or ID here" className="text-black z-10 indent-2 grow rounded-lg py-1" onChange={onChange} ref={inputRef} aria-label="TextField: Enter your Spotify's playlist ID or URL here"/>
                 {errorMsg && <InputErrorMsg  errorMsg={errorMsg}/>}
-                <button className="h-6 w-6 block text-gray-800 dark:text-white" type="submit" aria-label="Button: generate response">
+                <button className="h-6 w-6 block text-gray-800 dark:text-white" type="submit" aria-label="Button: generate response" disabled={isButtonDisabled}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="100%" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16" aria-hidden>
                         <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
                     </svg>
